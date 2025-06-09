8fcf92bfcf08ad4c2ce99c7c66f6b90942ce2561
Victor Toro <142246775+LilkongW@users.noreply.github.com>
2025-06-09 14:39:40 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // 👇 Solo el path del repo, con slashes
 export default defineConfig({
-  base: '/pageToShow/',
+  base: '/',
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
